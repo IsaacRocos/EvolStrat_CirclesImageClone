c2a7911a7c6ca1d1c3127bef2ceba6293a772d84
Isaac Rodríguez <angelrc@live.com.mx>
2015-05-25 23:33:49 -0500

    Avance en Funcinalidad de estrategia evolutiva

--- src/Circulo.py
+++ src/Circulo.py
@@ -11,11 +11,8 @@ class Circulo(object):
     '''
     classdocs
     '''
-    
-    BACKGROUND_COLOR = (255, 255, 255)
-    DEFAULT_COLOR = (0, 0, 0)
-    DEFAULT_SIZE = 30
-    TRANSPARENT = (255,0,255)
+
+    TRANSPARENT = (100,0,100)
 
 #pygame.draw.circle(Surface, color, pos, radius, width=0): return Rect
 
@@ -25,30 +22,22 @@ class Circulo(object):
         '''
         self.windowWidth = width
         self.windowHeight = height
-        self.radio = r
         self.color = color
-        self.red   = 0
-        self.green = 0
-        self.blue  = 0
         self.coorX = x
         self.coorY = y
-        self.surf = pygame.Surface((width,height))
+        self.radio = r
+        self.surf = pygame.Surface((self.radio*2,self.radio*2))
         self.surf.fill(Circulo.TRANSPARENT)
-
 
         
     def display(self, display):
         self.surf.set_colorkey(Circulo.TRANSPARENT)
-        pygame.draw.circle(self.surf, self.color, (self.coorX, self.coorY), self.radio)
+        pygame.draw.circle(self.surf, self.color, (self.radio, self.radio), self.radio)
         self.surf.set_alpha(100)
-        #display.blit(self.surf, (self.location.x-self.r, self.location.y-self.r, 2*self.r, 2*self.r))
-        display.blit(self.surf, (self.coorX,self.coorY))
-        #self.checkEdges2()
+        display.blit(self.surf, (self.coorX-self.radio, self.coorY-self.radio, 2*self.radio, 2*self.radio))
         
+        
+    def __str__(self):
+        circle =  'R=',self.radio,' C=',self.color,' Coor= ',self.coorX,',',self.coorY 
+        return str(circle)
 
-    def checkEdges(self):
-        if self.coorX >= self.windowWidth or self.coorX <= 0:
-            self.velocity.x = self.velocity.x * -1
-
-        if self.coorY>= self.windowHeight or self.coorY<= 0:
-            self.velocity.y = self.velocity.y * -1
--- src/GeneradorImg.py
+++ src/GeneradorImg.py
@@ -6,6 +6,7 @@ Created on 24/05/2015
 import random, pygame, sys
 from pygame.locals import *
 from Circulo import Circulo
+from math import sqrt
 
 NUMBER_BALLS = 10
 
@@ -14,68 +15,125 @@ class Generador(object):
     classdocs
     '''
     RED   = (255,0,0)
-    WHITE = (255, 255, 255)
+    WHITE = (255, 255, 255,255)
     BLUE  = (0,0,220)
 
-    def __init__(self, WINDOWWIDTH,WINDOWHEIGHT,FPS):
-        '''
-        Constructor
-        '''
-        self.WINDOWWIDTH = WINDOWWIDTH
-        self.WINDOWHEIGHT = WINDOWHEIGHT
+    def __init__(self,FPS):
+        self.nombreImagen = ""
+        self.verificaArgumentos()
+#         self.WINDOWWIDTH = WINDOWWIDTH
+#         self.WINDOWHEIGHT = WINDOWHEIGHT
+        self.imagen  = pygame.image.load(self.nombreImagen)
+        self.WINDOWWIDTH = self.imagen.get_size()[0]
+        self.WINDOWHEIGHT = self.imagen.get_size()[1]
+        self.aptitud = 1000000
         self.FPS = FPS
         self.circulos = []
-
-    def setup(self):
-        self.circulos = [ 
-        Circulo(self.WINDOWWIDTH, self.WINDOWHEIGHT, (random.randint(0,255) , random.randint(0,255) , random.randint(0,255) , 100),x=random.randint(0,400),y=random.randint(0,400), r=random.randint(10,50)) 
-        for i in xrange(NUMBER_BALLS)]
         
+
+    def verificaArgumentos(self):
+        if len(sys.argv) <= 1:
+            self.nombreImagen = "circulo.png"
+        else:
+            self.nombreImagen = sys.argv[1]
+            print 'Abriendo <', self.nombreImagen , '> ...'
+        print 'Imagen a imitar: ', self.nombreImagen
+
+
     def nuevoCirculo(self):
         print 'Generando nuevo circulo'
-        self.circulos.append( Circulo(
+        col = random.randint(0,100) # para grises
+        nuevoCirculo = Circulo(
         self.WINDOWWIDTH, 
         self.WINDOWHEIGHT, 
-        (random.randint(0,255) , random.randint(0,255) , random.randint(0,255) ,  100),
-        x =random.randint(0,400),
-        y =random.randint(0,400), 
+        #(random.randint(0,255) , random.randint(0,255) , random.randint(0,255) ,  100), #para colores aleatorios
+        (col, col , col ,  100),
+        x =random.randint(0,self.WINDOWWIDTH),
+        y =random.randint(0,self.WINDOWHEIGHT), 
         r =random.randint(10,50)
         )
-        )
+        self.circulos.append(nuevoCirculo)
+        print nuevoCirculo
         
+    
     def borrarCirculo(self):
-        self.circulos.pop()
+        if(len(self.circulos)>0):
+            self.circulos.pop()
 
+    
     def dibujarCirculos(self):        
         for ball in self.circulos:            
             ball.display(self.DISPLAYSURF)
         pygame.display.flip()
 
     
+    def evaluarAptitud(self):
+        print 'Analizando nueva imagen ...'
+        sumM = 0
+        for x in range (self.WINDOWWIDTH): #leer pixeles hacia derecha
+            sumN = 0
+            for y in range (self.WINDOWHEIGHT): #leer pixeles hacia abajo 
+                pixelIMG = self.imagen.get_at((x,y))
+                pixelSURF = self.DISPLAYSURF.get_at((x,y))
+                nvopixel = self.restaPixel(pixelIMG, pixelSURF)
+                sumN += nvopixel 
+            sumM += sumN
+        return sqrt(sumM)
+            
+    
+    def restaPixel(self,pixelIMG,pixelSURF):
+        npr = pixelIMG.r - pixelSURF.r
+        npg = pixelIMG.g - pixelSURF.g 
+        npb = pixelIMG.b - pixelSURF.b
+        npa = pixelIMG.a - pixelSURF.a
+        return (npr**2 + npg**2 + npb**2  + npa**2 + 0.0) / 4.0
+
+    def actualizar(self):
+        self.DISPLAYSURF.fill(Generador.WHITE)
+        self.dibujarCirculos()
+        pygame.display.update()
+        FPSCLOCK.tick(self.FPS)
+    
+    
     def start(self):
-        print "----INICIO----"
-        # Inicializar ventana --------------------------------------------
+        print "Generando..."
+        # Inicializar ventana -----------
         global FPSCLOCK, DISPLAYSURF
         pygame.init()
         FPSCLOCK = pygame.time.Clock()
         self.DISPLAYSURF = pygame.display.set_mode((self.WINDOWWIDTH, self.WINDOWHEIGHT))
         self.DISPLAYSURF.fill(Generador.WHITE)
+        evaluar = 0
         while True:
             for event in pygame.event.get(): # event handling loop
                 if (event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE)):
                     pygame.quit()
                     print 'Fin de ejecucion'
                     sys.exit()
+                if event.type == MOUSEBUTTONUP:
+                    self.mousex, self.mousey = event.pos
+                    print "Click en: ",self.mousex,self.mousey,' RGBA:',self.DISPLAYSURF.get_at((self.mousex,self.mousey))
                 if (event.type == KEYUP and event.key == K_SPACE):
                     print 'Agregando circulo'
                     self.nuevoCirculo()
+                    evaluar = 1
                 if (event.type == KEYUP and event.key == K_BACKSPACE):
                     print 'Borrando anterior'
                     self.borrarCirculo()
-                self.DISPLAYSURF.fill(Generador.WHITE)
-                self.dibujarCirculos()()        
-                pygame.display.update()
-                FPSCLOCK.tick(self.FPS)
-
-prueba = Generador(800,800,30)
+                self.actualizar()
+                if(evaluar == 1):
+                    evaluar=0
+                    apt = self.evaluarAptitud()
+                    if(apt < self.aptitud):
+                        print 'H-APT:',apt,'<','P_APT:',self.aptitud
+                        self.aptitud = apt
+                        self.actualizar()
+                    else:
+                        print 'H-APT:',apt,' no supera a padre'
+                        self.borrarCirculo()
+                        self.actualizar()
+                        
+prueba = Generador(20)
 prueba.start()
+
+
